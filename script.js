bf725c740f0a9d255c9da19f91f60f8176f22025
thinley norbu <80090521+Thinley-bot@users.noreply.github.com>
2021-08-12 10:39:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function function_More(){
+
+    var x = document.getElementById("SecondTabs");
+
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+      } else {
+        x.style.display = "none";
+      }
+}
